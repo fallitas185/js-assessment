0b9ee7b54eab024103448aa57366107220bd9307
Cindy Fallas <fallitas185@gmail.com>
2014-03-13 09:32:48 -0600

    Flow control done

--- app/flowControl.js
+++ app/flowControl.js
@@ -11,6 +11,22 @@ define(function() {
       //
       // otherwise the function should return the number, or false if no number
       // was provided or the value provided is not a number
+
+      var result = '';
+      if (num % 3 == 0) {
+        result = 'fizz';
+      }
+      if (num % 5 == 0) {
+        result += 'buzz';
+      }
+      if (isNaN(num)) {
+        result = false;
+      }
+      if (result === '') {
+        result = num;
+      }
+
+      return result;
     }
   };
 });
